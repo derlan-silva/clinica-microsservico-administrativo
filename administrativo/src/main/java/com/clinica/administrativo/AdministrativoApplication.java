d80e7c483e0c5c838f0d7ddcbc58efe3667d2c2c
Derlan <derlan.silva@aluno.imepac.edu.br>
2026-06-27 10:07:02 -0300

    feat: adicionando configuracao do banco de dados

---
+++ administrativo/src/main/java/com/clinica/administrativo/AdministrativoApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.administrativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministrativoApplication.class, args);
+	}
+
+}
